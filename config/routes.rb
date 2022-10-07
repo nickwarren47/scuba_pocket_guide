1a2ae04861ef006121f8fa28c26bdcf568370ec5
Nick Warren <nickwarren47@gmail.com>
2022-10-07 14:31:25 -0600

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
   
-  resources :dive_logs
-  resources :dive_site_animals
-  resources :dive_sites, only: [:index, :show]
-  resources :animals, only: [:index, :show]
-  resources :divers
+    resources :dive_logs
+    resources :dive_site_animals
+    resources :dive_sites, only: [:index, :show]
+    resources :animals, only: [:index, :show]
+    resources :divers
 
-  get "/me", to: "divers#show_current"
-  post "/signup", to: "divers#create"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-
-
+    get "/me", to: "divers#show_current"
+    post "/signup", to: "divers#create"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    get "/dive_sites/:id/animals", to: "dive_sites#showAnimals"
+    # get "/divers/current", to: "divers#show_current"
+    get "/divers/:id/dive_logs", to: "divers#showDiverLogs"
+  end 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  get "/dive_sites/:id/animals", to: "dive_sites#showAnimals"
-  # get "/divers/current", to: "divers#show_current"
-  get "/divers/:id/dive_logs", to: "divers#showDiverLogs"
 end
